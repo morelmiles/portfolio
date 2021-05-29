1abb449067604913991e8ef2ebb3ae87ed671384
Luigi Morel <morelmiles@gmail.com>
2021-05-29 11:13:15 +0300

    Change success location

--- src/Success.js
+++ src/Success.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../navbar/Navbar';
+import Navbar from './components/navbar/Navbar';
 
 const Success = () => {
     return (
