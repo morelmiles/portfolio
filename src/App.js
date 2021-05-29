f943f6f816f137e416fbd91b6e0c30105a1cfd01
Luigi Morel <morelmiles@gmail.com>
2021-05-29 04:18:04 +0300

    Add meta information to the pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React from "react";
-import { Switch , Route} from "react-router";
-import BlogPage from "./pages/Blog";
+import React from 'react';
+import { Switch, Route } from 'react-router';
+import BlogPage from './pages/Blog';
 import HomePage from './pages/HomePage';
+import { Helmet } from 'react-helmet';
 
+<Helmet>
+    <meta
+        name="description"
+        content="Luigi Morel is a developer who is passionate about coding and best practices, startups and product design and a firm believer in the blockcahin technology. "
+    />
+</Helmet>;
 const App = () => {
-  return (
-      <>
-          <Switch>
-              <Route exact path="/" component={HomePage} />
-              <Route exact path="/blog" component={BlogPage} />
-          </Switch>
-      </>
-  );
-}
+    return (
+        <>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+                <Route exact path="/blog" component={BlogPage} />
+            </Switch>
+        </>
+    );
+};
 
-export default App;;
+export default App;
