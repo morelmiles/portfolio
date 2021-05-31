38af9741a914de0ea9ae281fdeea5a048e199ff9
Luigi Morel <morelmiles@gmail.com>
2021-05-31 13:01:09 +0300

    Remove the contact form

--- src/components/contactForm/Contact.js
+++ src/components/contactForm/Contact.js
@@ -17,68 +17,11 @@ function Contact() {
             <p className="text-center">
                 WhatsApp me on
                 <a href="tel:+256 704 916 696">+256 704 916 696</a> <br />
-                Or fill in the form below
+                Or email me on{' '}
+                <a href="mailto:morelmiles@gmail.com">
+                    morelmiles[at]gmail[dot]com
+                </a>
             </p>
-            <div className="container">
-                <form
-                    action="/success"
-                    onSubmit="submit"
-                    name="contact"
-                    data-netlify="true"
-                    method="post"
-                    data-netlify-honeypot="bot-field"
-                >
-                    <input type="hidden" name="form-name" value="contact" />
-
-                    <div hidden>
-                        <input name="bot-field" />
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 mb-5">
-                            <input
-                                type="text"
-                                className="form-control input__box"
-                                id="name"
-                                name="name"
-                                placeholder="Name"
-                                required
-                            />
-                            <div class="invalid-feedback">
-                                Valid name is required.
-                            </div>
-                        </div>
-                        <div className="col-md-6 mb-5">
-                            <input
-                                type="email"
-                                className="form-control input__box"
-                                id="email"
-                                name="email"
-                                placeholder="Email Address"
-                                autoComplete="email"
-                                required
-                            />
-                            <div class="invalid-feedback">
-                                Valid email address is required.
-                            </div>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-6 mb-2">
-                            <textarea
-                                className="form-control"
-                                rows="4"
-                                name="message"
-                                required
-                                placeholder="Message"
-                            ></textarea>
-                        </div>
-                    </div>
-
-                    <button type="submit" className="btnSubmit mt-3 mb-3 btn">
-                        Send Message
-                    </button>
-                </form>
-            </div>
         </>
     );
 }
