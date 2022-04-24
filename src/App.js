b62241e8fe4af4d578baf8ce8f4775ac8439ce17
Luigi Morel <morelmiles@gmail.com>
2022-04-24 20:17:14 +0300

    fix: helmet and organize imports

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import BlogPage from './pages/Blog';
-import HomePage from './pages/HomePage';
-import { Helmet } from 'react-helmet';
-import Success from './Success';
+import React from 'react'
+import { Switch, Route } from 'react-router'
+import { Helmet } from 'react-helmet'
 
-<Helmet>
-    <meta name="title" content="Luigi Morel" />
-    <meta
-        name="keywords"
-        content="Blockchain, product development, software development, product design, Uganda, Kampala web developers, "
-    />
-    <meta name="robots" content="index, follow" />
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="language" content="English" />
-    <meta name="revisit-after" content="1 days" />
-    <meta name="author" content="Luigi Morel" />
+// Components
+import BlogPage from './pages/Blog'
+import HomePage from './pages/HomePage'
+import Success from './Success'
 
-    <meta
-        name="description"
-        content="Luigi Morel is a developer who is passionate about coding and best practices, startups and product design and a firm believer in the blockcahin technology. "
-    />
-</Helmet>;
 const App = () => {
-    return (
-        <Switch>
-            <Route exact path="/" component={HomePage} />
-            <Route exact path="/blog" component={BlogPage} />
-            <Route exact path="/success" component={Success} />
-        </Switch>
-    );
-};
+  return (
+    <>
+      <Helmet>
+        <meta name="title" content="Luigi Morel" />
+        <meta
+          name="keywords"
+          content="Blockchain, product development, software development, product design, Uganda, Kampala web developers, "
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="Luigi Morel" />
 
-export default App;
+        <meta
+          name="description"
+          content="Luigi Morel is a developer who is passionate about coding and best practices, startups and product design and a firm believer in the blockcahin technology. "
+        />
+      </Helmet>
+
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/blog" component={BlogPage} />
+        <Route exact path="/success" component={Success} />
+      </Switch>
+    </>
+  )
+}
+
+export default App
