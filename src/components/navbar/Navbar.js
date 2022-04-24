2f6ddea745e2b3ba398c78991b2e269719a82227
Luigi Morel <morelmiles@gmail.com>
2022-04-24 20:19:28 +0300

    fix: format using new prettier rules

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,29 +1,26 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <div className="d-flex flex-column flex-md-row align-items-center nav__bar p-3 px-md-1  mb-3 ">
-            <Link
-                to="/"
-                className="my-0 mr-md-auto font-weight-normal name__logo"
-            >
-                L.M{' '}
-            </Link>
+  return (
+    <div className="d-flex flex-column flex-md-row align-items-center justify-between nav__bar p-3 px-md-1  mb-3 ">
+      <Link to="/" className="my-0 mr-md-auto font-weight-normal name__logo">
+        L.M
+      </Link>
 
-            <nav class="mx-auto" id="navBarResponsive">
-                <a
-                    href="https://drive.google.com/file/d/1zi2nXQLiauCgjo7NFlYvX-giFn-Kv7Qi/view?usp=sharing"
-                    target="_blank"
-                    rel="noreferrer noopenner"
-                    download
-                    className="p-4  nav__link"
-                >
-                    Resume
-                </a>
-            </nav>
-        </div>
-    );
-};
+      <nav class="mx-auto" id="navBarResponsive">
+        <a
+          href="https://drive.google.com/file/d/1zi2nXQLiauCgjo7NFlYvX-giFn-Kv7Qi/view?usp=sharing"
+          target="_blank"
+          rel="noreferrer noopenner"
+          download
+          className="p-4  nav__link"
+        >
+          Resume
+        </a>
+      </nav>
+    </div>
+  )
+}
 
-export default Navbar;
+export default Navbar
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,19 +3,17 @@ import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 
 const BlogPage = () => {
-    return (
-        <>
-            <Navbar />
-            <div className="jumbo__container ">
-                <div className="jumbotron ">
-                    <p className="text-center jumbo__text">
-                        Blog
-                    </p>
-                </div>
-            </div>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Navbar />
+      <div className="jumbo__container ">
+        <div className="jumbotron ">
+          <p className="text-center jumbo__text">Blog</p>
+        </div>
+      </div>
+      <Footer />
+    </>
+  )
 }
 
 export default BlogPage
